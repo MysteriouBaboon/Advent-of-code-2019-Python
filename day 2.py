6c173f28bc7e9f17a3ec9fccd8eef914f160a6b3
MysteriousBaboon <MysteriousBaboon@hotmail.com>
2019-12-14 07:35:35 +0100

    Not cleaned

--- day 2.py
+++ day 2.py
@@ -1,4 +1,4 @@
-'''''
+
 def bruteforce(verb, noun):
     list_intcode = [1, verb, noun, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 19, 1, 19, 5, 23, 1, 23, 5, 27, 2,
                     27, 10,
@@ -43,6 +43,6 @@ while True:
     else:
         verb += 1
         noun = 0
-        '''''
+
 
 
